6722c4e220df6a20ba94f215d8b3824da6445d84
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-02-13 20:57:04 +0900

    [Silver I] Title: 기타 레슨, Time: 312 ms, Memory: 23868 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,16 +1,20 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int[] A = new int[N];
         int start = 0;
         int end = 0;
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
             if (start < A[i]) {
                 start = A[i];
             }
