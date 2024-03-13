6f7096647df811b8a6152700cba252e142b37c06
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-03-13 14:08:30 +0900

    [level 1] Title: 최소직사각형, Time: 8.35 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int[] maxArray = new int[sizes.length];
-        int[] minArray = new int[sizes.length];
-        for(int i=0; i<sizes.length; i++){
+        //명함 번호의 작은 길이와 큰 길이를 나누고
+        //작은 길이 중 가장 큰 길이랑 큰 길이 중 가장 큰 길이의 곱
+        int[] maxArray = new int[sizes.length]; //큰 길이
+        int[] minArray = new int[sizes.length]; //작은 길이
+        for(int i=0; i<sizes.length; i++){ // 작은 길이 큰 길이 나누기 
             if(sizes[i][0]>sizes[i][1]){
                 maxArray[i] = sizes[i][0];
                 minArray[i] = sizes[i][1];
@@ -13,8 +15,10 @@ class Solution {
                 minArray[i] = sizes[i][0];
             }
         }
-        int max = Arrays.stream(maxArray).max().orElse(0);
-        int min = Arrays.stream(minArray).max().orElse(0);
+        Arrays.sort(maxArray);
+        Arrays.sort(minArray);
+        int max = maxArray[sizes.length - 1]; //큰 길이중 가장 큰 길이
+        int min = minArray[sizes.length - 1]; //작은 길이중 가장 큰 길이
         return max*min;
     }
 }
