fb82cdb92d80ef17330bd817fcd8718f61d6502e
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-03-14 20:31:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.98 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,23 +1,31 @@
 import java.util.*;
 
 class Solution {
+    int[] primeNumber = {};
     public int solution(int number, int limit, int power) {
-        int[] primeNumber = new int[number+1];
+        primeNumber = new int[number+1];
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                if(j==Math.sqrt(i)){
-                    primeNumber[i-1] ++;
-                }
-                else if (i % j == 0) {
-                    primeNumber[i-1] += 2;
-                }
-            }
+            findDivision(i,1);
         }
         for(int i=0; i<primeNumber.length; i++){
-            if(primeNumber[i]>limit){
-                primeNumber[i]=power;
+            if(primeNumber[i]>limit){ //배열의 각 원소가 limit보다 커지면
+                primeNumber[i]=power; //파워 넣어주기
             }
         }
         return Arrays.stream(primeNumber).sum();
+    }
+    
+    private void findDivision(int num, int i) {
+        if(i > Math.sqrt(num)) {
+            return;
+        }
+
+        if(Math.sqrt(num) == i) {
+            primeNumber[num - 1] += 1;
+        } else if (num % i == 0) {
+            primeNumber[num - 1] += 2;
+        }
+
+        findDivision(num, i+1);
     }
 }
