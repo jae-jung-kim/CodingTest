ac50ce4ccbacdb462f0dec04767a3c6c50c22544
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-03-15 17:10:20 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 10.65 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,3 +1,4 @@
+//https://rocket-dev.co.kr/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%88%98%EC%A7%91%EC%9C%A0%ED%9A%A8%EA%B8%B0%EA%B0%84-level1/
 import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
@@ -8,6 +9,10 @@ class Solution {
         for (int i = 0; i < terms.length; i++) {
             map.put(terms[i].split(" ")[0], terms[i].split(" ")[1]);
         }
+        //chat gpt의 가르침
+        //.은 정규 표현식에서 "모든 문자"를 의미하는 메타 문자입니다.
+        //문자열에서 .를 리터럴 문자로 사용하려면 이스케이프 처리가 필요합니다 (\.).
+        //Java에서는 \ 자체도 이스케이프 처리가 필요해서 \\로 써야 합니다 (\\.).
 
         int year = Integer.parseInt(today.split("\\.")[0]);
         int month = Integer.parseInt(today.split("\\.")[1]);
