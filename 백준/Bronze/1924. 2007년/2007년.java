df60d50f80edac362f9d2b8d82f7a1977c6a3568
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-03-27 12:12:49 +0900

    [Bronze I] Title: 2007년, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,12 +1,16 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int month = sc.nextInt();
-        int day = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
         String[] dayOfTheWeek = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
         int[] days = {0,31,59,90,120,151,181,212,243,273,304,334};
         int totalDay = days[month-1]+day;
