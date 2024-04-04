e1c329bbff910477ecb1fa1bf1474cbed2624eaf
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-04-04 11:47:42 +0900

    [Silver I] Title: 추월, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -1,25 +1,30 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Queue<String> queue = new LinkedList<>();
-        for(int i=0; i<N; i++){
-            queue.add(br.readLine());
-        }
 
-        int count = 0;
-        for(int i=0; i<N; i++){
-            String s = br.readLine();
-            if(!queue.peek().equals(s)){
-                count++;
-                queue.remove(s);
-            }else{
-                queue.poll();
-            }
-        }
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       int N = Integer.parseInt(br.readLine());
+       Queue<String> queue = new LinkedList<>();
+
+       for(int i=0; i<N; i++){
+           queue.offer(br.readLine());
+       }
+
+       int count = 0;
+       for(int i=0; i<N; i++){
+           String s = br.readLine();
+           if(!queue.peek().equals(s)){
+               queue.remove(s);
+               count++;
+           }else{
+               queue.poll();
+           }
+       }
         System.out.println(count);
     }
 }
