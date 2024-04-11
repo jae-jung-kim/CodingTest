0400a8bc682e47af8c8be85d7ab831c475aa8a7e
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-04-11 12:25:50 +0900

    [Silver III] Title: 거북이, Time: 412 ms, Memory: 23304 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.java
+++ 백준/Silver/8911. 거북이/거북이.java
@@ -1,48 +1,55 @@
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
-public class Main{
-    
-    static int [] dx = {-1,0,1,0};
-    static int [] dy = {0,1,0,-1};
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
+
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+
         int T = Integer.parseInt(br.readLine());
-        
-        for(int i=0;i<T;i++) {
-            int minX=0, minY=0, maxX=0, maxY=0, dir=0, nowX=0, nowY=0; //초기값 북쪽
-            
+        for(int tc=0; tc<T; tc++){
+            int minX=0, minY=0, maxX=0, maxY=0, dir=0, nowX=0, nowY=0;
+            //dir은 보는 방향
+            //dir==0 -> 위쪽, dir==1 -> 오른쪽, dir==2 -> 아래쪽, dir==3 -> 왼쪽
+
             String command = br.readLine();
-            
-            for(int j=0;j<command.length();j++) {
-                char c = command.charAt(j);
-                
-                if(c=='F') { //한 눈금 앞으로
-                    nowX = nowX+dx[dir];
-                    nowY = nowY+dy[dir];
-                }else if(c=='B') { //한 눈금 뒤로
-                    nowX = nowX-dx[dir];
-                    nowY = nowY-dy[dir];
-                }else if(c=='L') { // 왼쪽으로 90도
-                    if(dir==0) dir=3;
-                    else dir--;
-                }else if(c=='R') { // 오른쪽으로 90도
-                    if(dir==3) dir=0;
-                    else dir++;
+
+            for(int i=0; i<command.length(); i++){
+                char c = command.charAt(i);
+                if(c=='F'){
+                    nowX += dx[dir];
+                    nowY += dy[dir];
+                }else if(c=='B'){
+                    nowX -= dx[dir];
+                    nowY -= dy[dir];
+                }else if(c=='L'){
+                    if(dir==0){
+                        dir=3;
+                    }else{
+                        dir--;
+                    }
+                }else if(c=='R'){
+                    if(dir==3){
+                        dir=0;
+                    }else{
+                        dir++;
+                    }
                 }
-                
+
                 minX = Math.min(minX, nowX);
                 minY = Math.min(minY, nowY);
                 maxX = Math.max(maxX, nowX);
                 maxY = Math.max(maxY, nowY);
             }
-            sb.append((Math.abs(minX)+Math.abs(maxX))*(Math.abs(minY)+Math.abs(maxY))+"\n");
+            sb.append((Math.abs(minX)+Math.abs(maxX))*(Math.abs(minY)+Math.abs(maxY))).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
- 
 }
