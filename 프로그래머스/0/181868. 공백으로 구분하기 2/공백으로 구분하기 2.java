633cc73a3d7af9e3e90a0040f309ba90e3ab8aba
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-11-26 16:38:24 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.49 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        StringTokenizer st = new StringTokenizer(my_string);
+        String[] answer = answer = new String[st.countTokens()];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] = st.nextToken();
+        }
+        return answer;
+    }
+}
