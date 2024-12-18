732eb833aa4fe4a193f874f5ea21126fe7122805
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-12-18 22:41:59 +0900

    [Bronze V] Title: 문자열, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
