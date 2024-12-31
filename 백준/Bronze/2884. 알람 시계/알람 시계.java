3070d56f30ad2ad930ddd609e4e7142f5a549113
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2024-12-31 17:55:44 +0900

    [Bronze III] Title: 알람 시계, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int H = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
@@ -22,6 +21,7 @@ public class Main {
         if(totalH==24){
             totalH=0;
         }
-        System.out.println(totalH + " " + totalM);
+        sb.append(totalH).append(" ").append(totalM);
+        System.out.println(sb.toString());
     }
 }
